2d3060d331bcf398a3aa5d5fe8bd8ddc81e666b5
dawtom97 <daw.tomczyk1997@gmail.com>
2023-07-29 13:25:47 +0200

    login and cors implementation

--- src/app.py
+++ src/app.py
@@ -4,12 +4,18 @@ from config import users_collection
 from utils.regex_patterns import password_pattern, email_pattern
 from utils.session_expiration import set_session_expiration
 from dotenv import load_dotenv
+from utils.get_response import get_response
+from flask_cors import CORS
 import datetime
 import re
 import os 
 load_dotenv()
 
 app = Flask(__name__)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 app.secret_key = os.getenv("SECRET_KEY")
 
 # ---------------------------------------------------------------------------------------------------------
@@ -23,43 +29,19 @@ def register():
         hashed_password = generate_password_hash(password)
 
         if users_collection.find_one({'username': username}):
-            error_response = jsonify({
-                'message': 'Użytkownik o podanej nazwie już istnieje',
-                'ok': False,
-                'status': 500
-            })
-            error_response.status_code = 500
-            return error_response
+            return get_response('Użytkownik o podanej nazwie już istnieje',False,500)
 
         # Sprawdzenie czy email istnieje już w bazie
         if users_collection.find_one({'email': email}):
-            error_response = jsonify({
-                'message': 'Konto o podanym adresie email już istnieje',
-                'ok': False,
-                'status': 500
-            })
-            error_response.status_code = 500
-            return error_response
+            return get_response('Konto o podanym adresie email już istnieje',False,500)
 
         # Sprawdzenie hasła za pomocą regular expression
         if re.match(password_pattern, password) is None:
-            error_response = jsonify({
-                'message': 'Hasło musi zawierać małą, dużą literę, cyfrę i minimum 5 znaków',
-                'ok': False,
-                'status': 500
-            })
-            error_response.status_code = 500
-            return error_response
+            return get_response('Hasło musi zawierać małą, dużą literę, cyfrę i minimum 5 znaków',False,500)
         
         # Sprawdzenie emaila za pomocą regular expression
         if re.match(email_pattern, email) is None:
-            error_response = jsonify({
-                'message': 'Niepoprawny adres email',
-                'ok': False,
-                'status': 500
-            })
-            error_response.status_code = 500
-            return error_response
+            return get_response('Niepoprawny adres email',False,500)
 
         # Dodanie nowego dokumentu do bazy
         users_collection.insert_one({
@@ -69,15 +51,12 @@ def register():
         })
 
         # Wyświetlenie czegoś po pomyślnym wyślaniu zapytania o rejestrację
-        response = jsonify({
+        new_user = {
             'username': username,
             'password': hashed_password,
             'email': email
-        })
-        response.status_code = 201
-
-        return response
-    
+        }
+        return get_response("Utworzono konto",True,201,new_user)
 
 #-------------------------------------------------------------------------------
 # Logowanie
@@ -92,20 +71,34 @@ def login():
          expiration = set_session_expiration(app)
          session['email'] = user['email']
          session['date'] = (datetime.datetime.now() + expiration).strftime('%H:%M:%S')
-    
-         response = jsonify({
-             'message':'Poprawnie zalogowano',
-             'ok':True,
-             'status':200
-         })
-         response.status_code = 200
-         return response
-     
-     error_response = jsonify({
-        'message': 'Błędny email lub hasło',
-        'ok': False,
-        'status': 403
-     })
-     error_response.status_code = 403
-     return error_response
 
+         return get_response('Poprawnie zalogowano',True,200)
+     
+     return get_response('Błędny email lub hasło',False,403)
+
+
+# -----------------------------------------------------------------------------------------------
+# Panel użytkownika
+@app.route('/dashboard',methods=['GET'])
+def dashboard():
+    if 'email' in session:
+        user = users_collection.find_one({'email':session['email']})
+        return get_response('Zalogowano jako ' + user['username'],True,200)
+    else:
+     return get_response('Odmowa dostępu',False,403)
+    
+
+# Wylogowanie
+@app.route('/logout')
+def logout():
+
+    session.pop('email',None)
+    session.pop('date',None)
+    return get_response('Pomyślnie wylogowano',True,200)
+
+
+# Wyświetlanie wszystkich userów
+@app.route('/get-users')
+def get_users():
+    users = users_collection.find({},{'_id':0})
+    return jsonify(list(users))
